a3d6d9f0c4a874ef7ae969487d2f33439020b4ef
Jason Pike <jason.pike@stellasource.com>
2024-05-15 15:57:51 -0500

    adding expand

--- Point.cs
+++ Point.cs
@@ -1,5 +1,5 @@
 public struct Point
 {
-    public int X { get; set; }
-    public int Y { get; set; }
+    public float X { get; set; }
+    public float Y { get; set; }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddOpenApiDocument(config =>
@@ -17,6 +19,7 @@ app.UseSwaggerUi(config =>
     config.DocExpansion = "list";
 });
 
+
 app.MapPost("/bounding-box", (Point[] points) =>
 {
     if (points.Length < 3)
@@ -24,10 +27,10 @@ app.MapPost("/bounding-box", (Point[] points) =>
         return Results.BadRequest("At least 3 points are required to create a bounding box.");
     }
     
-    var minX = int.MaxValue;
-    var maxX = int.MinValue;
-    var minY = int.MaxValue;
-    var maxY = int.MinValue;
+    var minX = float.MaxValue;
+    var maxX = float.MinValue;
+    var minY = float.MaxValue;
+    var maxY = float.MinValue;
 
     foreach (var pt in points)
     {
@@ -50,6 +53,44 @@ app.MapPost("/bounding-box", (Point[] points) =>
     };
     
     return Results.Ok(box);
+});
+
+app.MapPost("/bounding-box/expand", (BoundingBox boundingBox, [FromQuery]float percentage) =>
+{
+    if (percentage <= 0)
+    {
+        return Results.BadRequest("Percentage to expand must be greater than zero.");
+    }
+    
+    var multiplier = percentage / 100f;
+    var dxTop = Math.Abs(boundingBox.TopRight.X - boundingBox.TopLeft.X) * multiplier;
+    var dxBottom = Math.Abs(boundingBox.BottomRight.X - boundingBox.BottomLeft.X) * multiplier;
+    var dyRight = Math.Abs(boundingBox.TopRight.Y - boundingBox.BottomRight.Y) * multiplier;
+    var dyLeft = Math.Abs(boundingBox.TopLeft.Y - boundingBox.BottomLeft.Y) * multiplier;
+
+    return Results.Ok(new BoundingBox
+    {
+        TopRight = new Point
+        {
+            X = boundingBox.TopRight.X + dxTop,
+            Y = boundingBox.TopRight.Y + dyRight
+        },
+        TopLeft = new Point
+        {
+            X = boundingBox.TopLeft.X - dxTop,
+            Y = boundingBox.TopLeft.Y + dyLeft
+        },
+        BottomRight = new Point
+        {
+            X = boundingBox.BottomRight.X + dxBottom,
+            Y = boundingBox.BottomRight.Y - dyRight
+        },
+        BottomLeft = new Point
+        {
+            X = boundingBox.BottomLeft.X - dxBottom,
+            Y = boundingBox.BottomLeft.Y - dyLeft
+        },
+    });
 });
 
 app.Run();
